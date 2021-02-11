57433a317fb63cd660bf25d577c512cdce810096
jonathan <joanathan.meneses@walmart.com>
2021-02-11 13:42:42 -0300

    [Jonathan][POKE-002] organice imports and modify greadle file

--- src/main/java/com/pokemon/Pokedex/PokedexApplication.java
+++ src/main/java/com/pokemon/Pokedex/PokedexApplication.java
@@ -1,15 +1,5 @@
 package com.pokemon.Pokedex;
 
-import java.util.Collection;
-import java.util.List;
-import me.sargunvohra.lib.pokekotlin.client.PokeApi;
-import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
-import me.sargunvohra.lib.pokekotlin.model.Characteristic;
-import me.sargunvohra.lib.pokekotlin.model.EvolutionChain;
-import me.sargunvohra.lib.pokekotlin.model.NamedApiResource;
-import me.sargunvohra.lib.pokekotlin.model.NamedApiResourceList;
-import me.sargunvohra.lib.pokekotlin.model.Pokemon;
-import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
